3c52a21e2a25a01b76867b79e3f6a1d38be2da20
feiq <inchingcode@gmail.com>
2018-09-17 08:38:55 +0800

    Site updated: 2018-09-17 08:38:52

--- js/page/product.js
+++ js/page/product.js
@@ -1,1 +1,29 @@
-window.addEventListener("load",function(){var a=document.querySelector("article.page__post");new Pack(a).base("js-ease-out-leave-active").base("js-ease-out-leave").transfrom("js-ease-out-enter-active").end(function(){["js-ease-out-enter","js-ease-out-enter-active","js-ease-out-leave","js-ease-out-leave-active"].forEach(function(e){a.classList.remove(e)})}).toggle(),$("#image-gallery").lightSlider({gallery:!0,item:1,thumbItem:9,slideMargin:0,speed:500,auto:!1,loop:!1,onSliderLoad:function(){$("#image-gallery").removeClass("cS-hidden")}})});
+window.addEventListener('load', function() {
+    var postContent = document.querySelector('article.page__post');
+
+    new Pack(postContent)
+        .base('js-ease-out-leave-active')
+        .base('js-ease-out-leave')
+        .transfrom('js-ease-out-enter-active')
+        .end(function() {
+            var arr = ['js-ease-out-enter', 'js-ease-out-enter-active', 'js-ease-out-leave', 'js-ease-out-leave-active'];
+
+            arr.forEach(function(item) {
+                postContent.classList.remove(item);
+            });
+        })
+        .toggle();
+
+    $('#image-gallery').lightSlider({
+        gallery:true,
+        item:1,
+        thumbItem:9,
+        slideMargin: 0,
+        speed:500,
+        auto:false,
+        loop:false,
+        onSliderLoad: function() {
+            $('#image-gallery').removeClass('cS-hidden');
+        }  
+    });
+});
